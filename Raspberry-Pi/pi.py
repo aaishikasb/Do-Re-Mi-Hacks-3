366f0beca7e9cda7201eeb9cb19b79561b9291c6
Aaishika S Bhattacharya <52964353+aaishikasb@users.noreply.github.com>
2022-11-12 16:17:26 +0530

    🚩 Replace "channel" with "audio"

--- Raspberry-Pi/pi.py
+++ Raspberry-Pi/pi.py
@@ -4,16 +4,16 @@ import time
 
 audio = 17
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(audio, GPIO.IN)
 
-def callback(channel):
-    if GPIO.input(channel):
+def callback(audio):
+    if GPIO.input(audio):
       print "High"
     else:
       print "High"
       
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
-GPIO.add_event_callback(channel, callback)
+GPIO.add_event_detect(audio, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(audio, callback)
 
 while True:
   time.sleep(1)
