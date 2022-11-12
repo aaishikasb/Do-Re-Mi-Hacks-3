7859137aeaa36ff255ecc601f7d53504ca584499
Aaishika S Bhattacharya <52964353+aaishikasb@users.noreply.github.com>
2022-11-12 18:58:00 +0530

    📱 Integrate Twilio

--- Raspberry-Pi/pi.py
+++ Raspberry-Pi/pi.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python
+from twilio.rest import Client
 import RPi.GPIO as GPIO
 import time
 
 audio = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(audio, GPIO.IN)
+
+account_sid = '$SID'
+auth_token = '$AUTH'
+client = Client(account_sid, auth_token)
+
+def Msg(channel): 
+   print("Detected, sending notification.")
+   message = client.messages \
+    .create(
+         body='Hi, your nearby sound levels have crossed the limit set by you.',
+         from_='+19124612542',
+         to='+917827794110'
+     )
+
+   print(message.sid)
 
 def callback(audio):
     if GPIO.input(audio):
@@ -14,6 +30,7 @@ def callback(audio):
       
 GPIO.add_event_detect(audio, GPIO.BOTH, bouncetime=300)
 GPIO.add_event_callback(audio, callback)
+GPIO.add_event_callback(channel, Msg)
 
 while True:
   time.sleep(1)
